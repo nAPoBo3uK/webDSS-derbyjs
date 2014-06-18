634fa8b1f423fef97bf044a4b36329421d440287
nAPoBo3uK <bdenmail@gmail.com>
2014-06-18 15:33:08 +0300

    make candidats table add/delete cols/rows

--- src/app/candidats.js
+++ src/app/candidats.js
@@ -5,53 +5,83 @@ var Candidats = function(){
 
 }
 
-Candidats.prototype.addCandidat = function(){
+Candidats.prototype.addRow = function(){
     console.log('newRow');
     var table = this.model.at('table');
     var matrix = table.get();
-    if(matrix && matrix.length) {
-        var emptyRow = [];
-        for (var i = 0; i < matrix[0].length; i++)
-            emptyRow.push('');
-        console.log(emptyRow);
-        table.push(emptyRow);
-    } else {
-        table.push(['']);
-
-    }
-
-
+    var emptyRow = [];
+    for (var i = 0; i < matrix[0].length; i++)
+        emptyRow.push('');
+    console.log(emptyRow);
+    table.push(emptyRow);
 }
 
-Candidats.prototype.addCriteria = function(){
-    console.log('add criteria');
+Candidats.prototype.addCol = function(){
+    console.log('add col');
     var table = this.model.at('table');
     var matrix = table.get();
     if(matrix && matrix.length){
         for(var i=0; i< matrix.length; i++)
             table.at(i).push('');
     }
+    console.log(this.model.get());
 }
 
-Candidats.prototype.delCandidat = function(row){
-    console.log('del candidat ' + row);
+Candidats.prototype.delRow = function(row){
+    console.log('del row ' + row);
     var table = this.model.at('table');
     console.log(table.get());
-    table.set(table.get().filter(function(item, index){index!=row}));
+    table.set(table.get().filter(function(item, index){return index!=row}));
 
 }
-Candidats.prototype.delCrireria = function(col){
-    console.log('del candidat ' + row);
+
+Candidats.prototype.delCol = function(col){
+    console.log('del col ' + col);
     var table = this.model.at('table');
     var matrix = table.get();
-    for(var i= 0; i<matrix.length;i++)
-        table.del(i + '.' + col);
+
+    table.set(matrix.map(function(item){
+        return item.filter(function(item, index){
+            return index != col;
+        })
+    }));
 
 }
 
 Candidats.prototype.init = function(){
     console.log('candidats component init');
-      console.log(this.model.get());
+    this.model.at('table')
+    var table = this.model.at('table');
+    var matrix = table.get();
+    if(!matrix || !matrix.length) {
+        table.push(['']);
+    }
+}
+
+Candidats.prototype.editCell = function(cell, row, col){
+
+    this.model.set('edit', {
+        row: row,
+        col: col,
+        text: cell
+    });
+
+    window.getSelection().removeAllRanges();
+    document.getElementById('cell'+ row + col).focus()
+}
+
+Candidats.prototype.doneEditing = function(){
+    console.log('done edit');
+    var cell = this.model.get('edit');
+    this.model.set('table.' + cell.row + '.' + cell.col, cell.text);
+    this.model.del('edit');
+}
+
+Candidats.prototype.cancelEditing = function(e){
+    // 27 = ESQ-key
+    if (e.keyCode == 27) {
+        this.model.del('edit');
+    }
 }
 
 module.exports = Candidats;
