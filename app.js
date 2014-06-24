d83b49b07f5b7e10278a58261d996eabf1e038f4
nAPoBo3uK <bdenmail@gmail.com>
2014-06-24 12:44:31 +0300

    all processing user environment replaced to app.js

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 /**
  * Created by bolehivsky on 08.06.14.
  */
-var options = {
-    static: __dirname + '/views/public'
-};
-
 var derby = require('derby');
 var app = require(__dirname + '/src/app');
+var server = require(__dirname + '/src/server/server.js');
+var http = require('http');
 
 function createServer() {
-    var server_port = options.port || process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000;
+    var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000;
     var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
     var mongoUrl;
     if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
         mongoUrl = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
@@ -24,13 +23,11 @@ function createServer() {
             || 'mongodb://localhost:27017/web-dss';
     }
 
-    var expressApp = require(__dirname + '/src/server/server.js').setup(app, mongoUrl);
-
-    var server = require('http').createServer(expressApp);
-    server.listen(server_port, function (e) {
-        if(e) {
-            console.log(e);
-        } else console.log('%d listening. Go to: http://localhost:%d/', process.pid, server_port);
-    });
+    http.createServer(server.setup(app, mongoUrl))
+        .listen(server_port, function (e) {
+            if(e) {
+                console.log(e);
+            } else console.log('%d listening. Go to: http://localhost:%d/', process.pid, server_port);
+        });
 }
 derby.run(createServer);
