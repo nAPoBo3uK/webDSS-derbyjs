6801fbd7710687795859067762461b3362a76260
nAPoBo3uK <bdenmail@gmail.com>
2014-07-12 16:08:22 +0300

    https mode added

--- app.js
+++ app.js
@@ -2,26 +2,48 @@
  * Created by bolehivsky on 08.06.14.
  */
 var derby = require('derby');
-var app = require(__dirname + '/src/app');
-var server = require(__dirname + '/src/server/server.js');
+var express = require('express');
+var initServer = require(__dirname + '/src/server/server.js');
+var https = require('https');
 var http = require('http');
-
-function createServer() {
-    var server_port = process.env.PORT || 3000;
-    var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
-    var mongoUrl;
-
-    mongoUrl = process.env.MONGO_URL
-        || process.env.MONGOHQ_URL
-        || 'mongodb://localhost:27017/web-dss';
+var fs = require('fs');
+var crypto = require('crypto');
 
 
-    http.createServer(server.setup(app, mongoUrl))
-        .listen(server_port, function (e) {
+derby.run(function () {
+    // options
+    var serverPort = process.env.PORT || 3000,
+        httpsPort =  process.env.HTTPS_PORT || (parseInt(serverPort)+1),
+        serverIp = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+        mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/web-dss',
+        sessionSecret = process.env.SESSION_SECRET || crypto.randomBytes(16).toString(),
+        key = process.env.KEY || 'server.key',
+        cert = process.env.PEM || 'server.pem';
+
+    var expressAppHTTP = express();
+    try {
+        var httpsOptions = {
+            key: fs.readFileSync(key),
+            cert: fs.readFileSync(cert),
+            requestCert: true
+        };
+
+        expressAppHTTP.get('*', function(req,res){
+            res.redirect('https://' + serverIp + ':' + httpsPort + req.url);
+        });
+        var expressAppHTTPS = express();
+        https.createServer(httpsOptions,  initServer(expressAppHTTPS, mongoUrl, sessionSecret))
+            .listen(httpsPort); // sequred server
+    } catch (e){
+        console.log(e.message);
+        console.log('No certificate. HTTPS connection unavailable.')
+        initServer(expressAppHTTP, mongoUrl, sessionSecret);
+    }
+    var server = http.createServer( expressAppHTTP ); // normal server
+        server.listen(serverPort, function (e) {
             if(e) {
                 console.log(e);
-            } else console.log('%d listening. Go to: http://localhost:%d/', process.pid, server_port);
+            } else console.log('%d listening. Go to: http://localhost:%d/', process.pid, serverPort);
         });
-}
-derby.run(createServer);
+})
+
--- src/server/server.js
+++ src/server/server.js
@@ -1,38 +1,34 @@
-// Express 4
-var express = require('express');
+
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var authMiddleware = require('./auth');
 var path = require('path');
+
 // Derby
 var derby = require('derby');
-
+var derbyApp = require('../app');
+var express = require('express');
 var racerBrowserChannel = require('racer-browserchannel');
 var liveDbMongo = require('livedb-mongo');
 
 derby.use(require('racer-bundle'));
 
 
-exports.setup = function (app, mongoUrl) {
-
-
+module.exports =  function (expressApp, mongoUrl, sessionSecret) {
     var store = derby.createStore({db: liveDbMongo(mongoUrl + '?auto_reconnect', {safe: true})});
 
-    var expressApp = express();
+
 
     // Respond to requests for application script bundles
-    expressApp.use(app.scripts(store));
+    expressApp.use(derbyApp.scripts(store));
 
-  /* options && options.static
-        && expressApp.use(require('serve-static')(options.static));
-*/
     expressApp.use(racerBrowserChannel(store));
     expressApp.use(store.modelMiddleware());
     expressApp.use(express.static(path.join(__dirname, '../../public')));
     expressApp.use(require('cookie-parser')());
     expressApp.use(session({
-        secret: process.env.SESSION_SECRET || 'YOUR SECRET HERE'
-        , store: new MongoStore({url: mongoUrl})
+        secret: sessionSecret || 'YOUR SECRET HERE',
+         store: new MongoStore({url: mongoUrl})
     }));
 
     expressApp.use(require('body-parser')());
@@ -40,7 +36,7 @@ exports.setup = function (app, mongoUrl) {
 
     expressApp.use(authMiddleware(store));
 
-    expressApp.use(app.router());
+    expressApp.use(derbyApp.router());
 
 
     expressApp.all('*', function(req, res, next) {
